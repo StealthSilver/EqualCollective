5bfd548a36e6b715f9137f6e8c4fc2e067b2e1e6
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-11-15 09:59:13 +0530

    implemented loader

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,29 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/"],
+        disallow: [
+          "/api/",
+          "/_next/",
+          "/admin/",
+          "/private/",
+          "*.json",
+        ],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/_next/",
+        ],
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/_next/",
+        ],
       },
     ],
     sitemap: `${SITE.url}/sitemap.xml`,
